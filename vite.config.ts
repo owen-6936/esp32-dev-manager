34c695b15ccc7676690e0ac3718e3fb1d542ac91
Owen Erhabor <erhaborowen3@gmail.com>
2025-08-19 09:42:35 +0100

    fix: add maximum file size limit for cache in workbox configuration

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,7 @@ export default defineConfig({
         ],
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB
         runtimeCaching: [
           {
             urlPattern: ({ url }) =>
